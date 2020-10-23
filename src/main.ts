443327bd7e22954797fb1ddc86ef8e0b57751e97
Subhrajyoti <subha.ionic3@gmail.com>
2020-10-23 10:04:24 +0530

    added unsubscrib and subscription

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs";
+import { Observable, Subscription, of } from "rxjs";
 
 const observable = new Observable(subscriber => {
     subscriber.next(1);
@@ -8,9 +8,18 @@ const observable = new Observable(subscriber => {
     }, 1000);
 });
 console.log("Start");
-observable.subscribe({
+const parentSubscription: Subscription = observable.subscribe({
     next(x) { console.log("Value is => ", (x)) },
     error(err) { console.log("Some error occurred ", err); },
     complete() { console.log("subscriber completed") }
 });
 console.log("End!!");
+
+const childSubscription: Subscription = of(1, 2, 3).subscribe(value => console.log("child process value => ", value));
+
+const subscription = parentSubscription.add(childSubscription);
+
+setTimeout(() => {
+    subscription.unsubscribe();
+    console.log("unsubscribed both!!");
+}, 3000)
